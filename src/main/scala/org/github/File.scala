243ee202c5404ed1da8d75e2f3cddb0088e9bbb5
Nuno Pássaro <nunomcpassaro@gmail.com>
2016-08-09 16:48:27 +0100

    :art: Add class method to delete the results file

--- src/main/scala/org/github/File.scala
+++ src/main/scala/org/github/File.scala
@@ -2,6 +2,10 @@ package org.github
 
 import scala.tools.nsc.io._
 
+object ResultsWriter {
+  def deleteResultsFile(path: String) = File(path).delete()
+}
+
 class ResultsWriter(filePathC: String, contentsC: List[String]) {
   var filePath = filePathC
   var contents = contentsC
